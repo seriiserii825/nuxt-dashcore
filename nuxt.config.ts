f06885b70f5813bc5864830969188c778b120462
serii burduja <seriiburduja@gmail.com>
2025-07-18 11:05:09 +0300

    feat: connect tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@pinia/nuxt", "@pinia/nuxt"],
+  modules: ["@pinia/nuxt", "@pinia/nuxt", "@nuxtjs/tailwindcss"],
 });
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="index"></div>
+  <div class="index">
+    <!-- create a block with tailwind css 3 -->
+    <div class="bg-blue-500 text-white p-4 rounded-lg shadow-lg">
+      <h1 class="text-2xl font-bold">Welcome to My Page</h1>
+      <p class="mt-2">This is a simple block styled with Tailwind CSS.</p>
+
+      <button class="mt-4 bg-white text-blue-500 px-4 py-2 rounded hover:bg-gray-100">
+        Click Me
+      </button>
+    </div>
+  </div>
 </template>
