d446ee324b86cd0d171e5d58691cf6141d0da337
serii burduja <seriiburduja@gmail.com>
2025-07-18 11:33:26 +0300

    feat: set tailwind colors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@pinia/nuxt", "@pinia/nuxt", "@nuxtjs/tailwindcss"],
+  css: ["@/assets/scss/main.scss"],
 });
-
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        accent: "#3c8dbc",
+        "accent-300": "#26374c",
+        "accent-200": "rgba(0,0,0, .9)",
+        "accent-100": "#182330",
+      },
+    },
+  },
+};
