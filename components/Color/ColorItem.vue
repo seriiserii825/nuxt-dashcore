6346020ee2a318b62d7021991f9fd6abc367795b
serii <seriiburduja@gmail.com>
2025-09-06 09:05:50 +0300

    feat: colors component

---
+++ components/Color/ColorItem.vue
@@ -0,0 +1,30 @@
+<script setup>
+	const props = defineProps({
+		color: {
+			type: String,
+			required: true,
+		},
+	});
+</script>
+<template>
+	<li class="color-item">
+		<span
+			class="color-item__value"
+			:style="{ background: `var(--${color})` }"
+		></span>
+		<span class="color-item__title">{{ color }}</span>
+	</li>
+</template>
+<style lang="scss" scoped>
+	.color-item {
+		display: flex;
+		align-items: center;
+		margin-bottom: 1rem;
+    cursor: pointer;
+		&__value {
+			margin-right: 1rem;
+			width: 2rem;
+			height: 2rem;
+		}
+	}
+</style>
---
+++ components/Color/ColorView.vue
@@ -0,0 +1,37 @@
+<script setup>
+const code = `
+:root {
+	--accent: #65B244;
+	--accent-hover: #a86078;
+	--contrast: #f16529;
+	--contrast-hover: #fa7134;
+	--dark-bg: #444;
+	--dark-bg-darken: #333;
+	--primary: #0d7296;
+	--primary-hover: #0f8dbb;
+	--success: #139b2d;
+	--success-hover: #19c73a;
+	--error: #980404;
+}
+`;
+
+</script>
+
+<template>
+  <div class="color-view">
+    <SectionHeader title="Colors"/>
+    <pre><code>{{ code }}</code></pre>
+    <ul class="colors__list">
+      <ColorItem color="accent"/>
+      <ColorItem color="accent-hover"/>
+      <ColorItem color="contrast"/>
+      <ColorItem color="contrast-hover"/>
+      <ColorItem color="dark-bg"/>
+      <ColorItem color="dark-bg-darken"/>
+      <ColorItem color="primary"/>
+      <ColorItem color="primary-hover"/>
+      <ColorItem color="success"/>
+      <ColorItem color="success-hover"/>
+    </ul>
+  </div>
+</template>
