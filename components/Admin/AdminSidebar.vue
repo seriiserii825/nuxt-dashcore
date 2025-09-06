6dec31e476f8ac02ca3aded060f1886c22e6dd3b
serii <seriiburduja@gmail.com>
2025-09-06 09:50:07 +0300

    feat: admin sidebar logo

--- components/Admin/AdminSidebar.vue
+++ components/Admin/AdminSidebar.vue
@@ -1,13 +1,13 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="admin-sidebar">
-
+    <img src="/images/app_header_logo.png" alt="" class="admin-sidebar__logo" />
   </div>
 </template>
 <style scoped lang="scss">
 .admin-sidebar {
+  padding: 3.2rem;
   height: 100%;
   background-color: #26374c;
 }
