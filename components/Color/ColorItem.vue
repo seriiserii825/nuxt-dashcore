c9db5009d99203483dd4cfa5da63e4ce339fe2d4
serii <seriiburduja@gmail.com>
2025-09-06 15:58:07 +0300

    feat: styled colors

--- components/Color/ColorItem.vue
+++ components/Color/ColorItem.vue
@@ -4,6 +4,10 @@
 			type: String,
 			required: true,
 		},
+    hex: {
+      type: String,
+      required: true,
+    }
 	});
 </script>
 <template>
@@ -12,15 +16,23 @@
 			class="color-item__value"
 			:style="{ background: `var(--${color})` }"
 		></span>
-		<span class="color-item__title">{{ color }}</span>
+		<h3 class="color-item__title">{{ color }}</h3>
+    <span class="color-item__hex">{{ hex }}</span>
 	</li>
 </template>
 <style lang="scss" scoped>
 	.color-item {
 		display: flex;
+    justify-content: space-between;
 		align-items: center;
 		margin-bottom: 1rem;
     cursor: pointer;
+    width: 30rem;;
+    &__title {
+      font-weight: normal;
+      text-align: left;
+      flex: 1;
+    }
 		&__value {
 			margin-right: 1rem;
 			width: 2rem;
--- components/Color/ColorView.vue
+++ components/Color/ColorView.vue
@@ -1,40 +1,20 @@
-<script setup>
-const code = `
-:root {
-  --accent: #3c8dbc;
-	--accent-hover: #307197;
-  --accent-light: #00c0ef;
-  --success: #00a65a;
-  --success-hover: #008d4c;
-  --error: #dd4b39;
-  --error-hover: #d73925;
-  --admin-bg: #f5f8fa;
-  --admin-bg-darken: #e4e9ec;
-  --text-color: #212529;
-  --text-color-light: #636b6f;
-}
-`;
-
-</script>
+<script setup></script>
 
 <template>
   <div class="color-view">
     <SectionHeader title="Colors"/>
-    <CodePreview className="css" class="mb-16">
-      {{ code }}
-    </CodePreview>
     <ul class="colors__list">
-      <ColorItem color="accent"/>
-      <ColorItem color="accent-hover"/>
-      <ColorItem color="accent-light"/>
-      <ColorItem color="success"/>
-      <ColorItem color="success-hover"/>
-      <ColorItem color="error"/>
-      <ColorItem color="error-hover"/>
-      <ColorItem color="admin-bg"/>
-      <ColorItem color="admin-bg-darken"/>
-      <ColorItem color="text-color"/>
-      <ColorItem color="text-color-light"/>
+      <ColorItem color="accent" hex="#3c8dbc"/>
+      <ColorItem color="accent-hover" hex="#307197"/>
+      <ColorItem color="accent-light" hex="#00c0ef"/>
+      <ColorItem color="success" hex="#00a65a"/>
+      <ColorItem color="success-hover" hex="#008d4c"/>
+      <ColorItem color="error" hex="#dd4b39"/>
+      <ColorItem color="error-hover" hex="#d73925"/>
+      <ColorItem color="admin-bg" hex="#f5f8fa"/>
+      <ColorItem color="admin-bg-darken" hex="#e4e9ec"/>
+      <ColorItem color="text-color" hex="#212529"/>
+      <ColorItem color="text-color-light" hex="#636b6f"/>
     </ul>
   </div>
 </template>
