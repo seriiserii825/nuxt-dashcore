9162015f9899a7e30d6a5c50a97d1d3c6709b12d
serii burduja <seriiburduja@gmail.com>
2025-09-08 12:18:30 +0300

    feat: need to finish account

---
+++ components/Account/AccountInfo.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="account-info">
+    <div class="account-info__top">
+      <div class="account-info__left">
+        <h3 class="account-info__name">{{ name }}</h3>
+        <div class="account-info__role">{{ role }}</div>
+      </div>
+      <div class="account-info__icon">
+        <FontAwesomeIcon icon="fa-s fa-user" />
+      </div>
+    </div>
+    <div class="account-info__bottom">
+      <FontAwesomeIcon icon="faw fa-sign-out" />
+    </div>
+  </div>
+</template>
--- components/Admin/AdminTopbar.vue
+++ components/Admin/AdminTopbar.vue
@@ -7,43 +7,49 @@ const admin_store = useAdminStore();
 <template>
   <div class="admin-topbar">
     <IBurger />
-    <h1>
+    <h1 class="admin-topbar__title">
       <span>{{ admin_store.level_1 }}</span>
       <small v-if="admin_store.level_2">{{ admin_store.level_2 }}</small>
     </h1>
+    <div class="admin-topbar__info">
+      <AccountInfo name="John Doe" role="Administrator" />
+    </div>
   </div>
 </template>
 <style lang="scss" scoped>
 .admin-topbar {
+  display: flex;
+  align-items: center;
+  padding: 15px 20px;
+  height: 8rem;
+  color: #444;
+  background: #fff;
+  border-bottom: 1px solid #d2d6de;
+  &__title {
     display: flex;
     align-items: center;
-    padding: 15px 20px;
-    height: 8rem;
-    color: #444;
-    background: #fff;
-    border-bottom: 1px solid #d2d6de;
-    h1 {
-      display: flex;
-      align-items: center;
-      gap: 0.8rem;
-      margin: 0 0 0 3.2rem;
-      span {
-        font-size: 2rem;
-        font-weight: 600;
-        color: var(--text-dark);
-      }
-      small {
-        position: relative;
-        top: 0.2rem;
-        font-size: 1.5rem;
-        font-weight: 400;
-      }
+    gap: 0.8rem;
+    margin: 0 0 0 3.2rem;
+    span {
+      font-size: 2rem;
+      font-weight: 600;
+      color: var(--text-dark);
     }
-    svg {
-      width: 24px;
-      height: 24px;
-      cursor: pointer;
-      fill: var(--text-dark);
+    small {
+      position: relative;
+      top: 0.2rem;
+      font-size: 1.5rem;
+      font-weight: 400;
     }
+  }
+  svg {
+    width: 24px;
+    height: 24px;
+    cursor: pointer;
+    fill: var(--text-dark);
+  }
+  &__info {
+    margin-left: auto;
+  }
 }
 </style>
