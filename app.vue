8a09fdbf864188d08d6ee7d3b0d7ded9888a9129
serii burduja <seriiburduja@gmail.com>
2025-09-08 09:55:28 +0300

    feat: page transition

--- app.vue
+++ app.vue
@@ -5,3 +5,14 @@
     </NuxtLayout>
   </div>
 </template>
+<style lang="scss">
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(0.3rem);
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
   compatibilityDate: "2025-07-15",
   ssr: false,
   devtools: { enabled: true },
   modules: ["@pinia/nuxt", "@nuxt/fonts", "@nuxt/eslint"],
   css: ["@/assets/scss/main.scss", "@fortawesome/fontawesome-svg-core/styles.css"],
 });
-
